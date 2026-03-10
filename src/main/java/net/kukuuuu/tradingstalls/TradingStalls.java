05a4a8231a199ec6c69c8a9b9517571d3dd31b6c
KuKuuu69 <kalana.sixtyknight@gmail.com>
2026-03-10 17:08:19 +0530

    Initial Commit

---
+++ src/main/java/net/kukuuuu/tradingstalls/TradingStalls.java
@@ -0,0 +1,20 @@
+package net.kukuuuu.tradingstalls;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TradingStalls implements ModInitializer {
+	public static final String MOD_ID = "trading-stalls";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/kukuuuu/tradingstalls/TradingStallsClient.java
@@ -0,0 +1,10 @@
+package net.kukuuuu.tradingstalls;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TradingStallsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
